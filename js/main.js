56c54fdd85ab8edd4ccf966ad6a6f6eadf9b44f4
Caity-G-OConnor <caity.g.oconnor@gmail.com>
2017-11-05 17:17:13 -0700

    Add quotes to array; change image area to div

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var quotes = [
+    'The best ideas come as jokes. Make your thinking as funny as possible. &mdash;David Ogilvy',
+    'Google has the functionality of a really complicated Swiss Army knife, but the home page is our way of approaching it closed. It&rsquo;s simple, it&rsquo;s elegant. You can slip it in your pocket, but it&rsqup;s got the great doodad when you need it. &mdash;Marissa Mayer',
+    'A beef filet cooked for 15 hours by 30 cooks doesn&rsquo;t necessarily taste better than a cheeseburger &mdash;Oliver Reichenstein',
+    'If you can&rsquo;t explain it simply, you don&rsquo;t understand it well enough. &mdash;Albert Einstein',
+    'You can use an eraser on the drafting table or a sledge hammer on the construction site. &mdash;Frank Lloyd Wright',
+    'Make things as simple as possible, but not simpler &mdash;Albert Einstein',
+    'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools. &mdashDouglas Adams',
+    'Think more. Design less. &mdash;Ellen Lupton'
+];
